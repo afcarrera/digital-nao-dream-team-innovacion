570ff2d6927c61f5b0313e9b2b6401def7ed179c
afcarrera <acarrera_l@hotmail.com>
2023-04-19 23:58:52 -0500

    Ajustes para documentación

---
+++ google-scholar/src/main/java/com/digitalnao/googlescholar/GoogleScholarApplication.java
@@ -0,0 +1,13 @@
+package com.digitalnao.googlescholar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleScholarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleScholarApplication.class, args);
+	}
+
+}
---
+++ google-scholar/src/test/java/com/digitalnao/googlescholar/GoogleScholarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalnao.googlescholar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleScholarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
